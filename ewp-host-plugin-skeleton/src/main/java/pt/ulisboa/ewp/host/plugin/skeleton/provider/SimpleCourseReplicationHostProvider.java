834052c1f687b139af189b1a81908a4ea685d6da
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2020-11-18 12:21:49 +0000

    feat: add provider to obtain list of learning opportunities specification IDs

---
+++ ewp-host-plugin-skeleton/src/main/java/pt/ulisboa/ewp/host/plugin/skeleton/provider/SimpleCourseReplicationHostProvider.java
@@ -0,0 +1,18 @@
+package pt.ulisboa.ewp.host.plugin.skeleton.provider;
+
+import java.util.Collection;
+
+/**
+ * A provider that allows EWP Node to obtain the list of Learning Opportunities Specification IDs from the host.
+ */
+public abstract class SimpleCourseReplicationHostProvider extends HostProvider {
+
+  /**
+   * Returns the list of Learning Opportunities Specification IDs for a given HEI ID.
+   * If the plugin cannot resolve the HEI ID then it must return an empty collection.
+   *
+   * @param heiId HEI ID of the institution
+   * @return List of Learning Opportunities Specification IDs or empty, if unknown HEI ID.
+   */
+  public abstract Collection<String> findAllByHeiId(String heiId);
+}
