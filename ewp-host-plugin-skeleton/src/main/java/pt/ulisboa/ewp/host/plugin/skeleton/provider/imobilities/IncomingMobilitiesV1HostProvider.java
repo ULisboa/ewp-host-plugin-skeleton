4f6d818536be8f5e5cad6090a73912209ee92216
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2021-11-11 11:12:19 +0000

    feat: add IncomingMobilitiesV1HostProvider

---
+++ ewp-host-plugin-skeleton/src/main/java/pt/ulisboa/ewp/host/plugin/skeleton/provider/imobilities/IncomingMobilitiesV1HostProvider.java
@@ -0,0 +1,42 @@
+package pt.ulisboa.ewp.host.plugin.skeleton.provider.imobilities;
+
+import java.util.Collection;
+
+import eu.erasmuswithoutpaper.api.imobilities.v1.endpoints.ImobilitiesGetResponseV1;
+import pt.ulisboa.ewp.host.plugin.skeleton.provider.HostVersionedProvider;
+
+/**
+ * A provider that allows EWP Node to obtain the list of Incoming Mobilities from the host (V1).
+ */
+public abstract class IncomingMobilitiesV1HostProvider extends HostVersionedProvider {
+
+    private static final int DEFAULT_MAX_NUMBER_OUTGOING_MOBILITY_IDS_PER_REQUEST = 1;
+
+    /**
+     * Returns a collection of Incoming Mobilities given their outgoing mobility IDs.
+     * If some ID is unknown then it must be ignored.
+     *
+     * @see <a href="https://github.com/erasmus-without-paper/ewp-specs-api-imobilities/blob/stable-v1/endpoints/get.md">EWP
+     *      Specification</a>
+     *
+     * @param receivingHeiId HEI ID of the receiving institution.
+     * @param outgoingMobilityIds Outgoing Mobilities IDs to obtain.
+     * @return Collection of Incoming Mobilities.
+     */
+    public abstract Collection<ImobilitiesGetResponseV1> findBySendingHeiIdAndOutgoingMobilityIds(String receivingHeiId,
+            Collection<String> outgoingMobilityIds);
+
+    /**
+     * Indicates the maximum number of Outgoing Mobility IDs that can be handled on any request.
+     * 
+     * @return Maximum number of Outgoing Mobility IDs
+     */
+    public int getMaxOutgoingMobilityIdsPerRequest() {
+        return DEFAULT_MAX_NUMBER_OUTGOING_MOBILITY_IDS_PER_REQUEST;
+    }
+
+    @Override
+    public String getVersion() {
+        return "1.0.0";
+    }
+}
