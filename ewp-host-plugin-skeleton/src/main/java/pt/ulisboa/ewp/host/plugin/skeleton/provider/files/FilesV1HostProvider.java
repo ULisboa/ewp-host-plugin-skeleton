444ffef5785da4ba87376f48466d4bb8e760699f
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2022-10-24 14:50:18 +0100

    feat: add requesterCoveredHeiIds parameter to FilesV1HostProvider

--- ewp-host-plugin-skeleton/src/main/java/pt/ulisboa/ewp/host/plugin/skeleton/provider/files/FilesV1HostProvider.java
+++ ewp-host-plugin-skeleton/src/main/java/pt/ulisboa/ewp/host/plugin/skeleton/provider/files/FilesV1HostProvider.java
@@ -1,5 +1,6 @@
 package pt.ulisboa.ewp.host.plugin.skeleton.provider.files;
 
+import java.util.Collection;
 import java.util.Optional;
 
 import pt.ulisboa.ewp.host.plugin.skeleton.provider.HostVersionedProvider;
@@ -16,10 +17,12 @@ public abstract class FilesV1HostProvider extends HostVersionedProvider {
      *
      * If the file requested does not exist then an Optional.empty() must be returned.
      *
+     * @param requesterCoveredHeiIds List of HEI IDs covered by the requester.
+     *                               The file must only be returned if the file ID was previously shared with the requester.
      * @param fileId ID of the file to return.
      * @return File data.
      */
-    public abstract Optional<FileResponse> getFile(String fileId);
+    public abstract Optional<FileResponse> getFile(Collection<String> requesterCoveredHeiIds, String fileId);
 
     @Override
     public String getVersion() {
