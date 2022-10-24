8d67346ad99aae14cf8d4bba815eeae2bf501884
Fábio Alexandre Canada Ferreira <fabioferreira@campus.ul.pt>
2022-10-24 13:41:22 +0100

    feat: update FactSheetsV1HostProvider to v1.1.0

--- ewp-host-plugin-skeleton/src/main/java/pt/ulisboa/ewp/host/plugin/skeleton/provider/factsheets/FactSheetsV1HostProvider.java
+++ ewp-host-plugin-skeleton/src/main/java/pt/ulisboa/ewp/host/plugin/skeleton/provider/factsheets/FactSheetsV1HostProvider.java
@@ -20,6 +20,6 @@ public abstract class FactSheetsV1HostProvider extends HostVersionedProvider {
 
     @Override
     public String getVersion() {
-        return "1.0.0";
+        return "1.1.0";
     }
 }
