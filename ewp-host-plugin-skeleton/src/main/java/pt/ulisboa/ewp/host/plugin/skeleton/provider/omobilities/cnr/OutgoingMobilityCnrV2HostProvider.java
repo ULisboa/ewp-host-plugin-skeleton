448f44136a5cf0390e502c191266278794cfb152
Fábio Ferreira <fabioferreira@edu.ulisboa.pt>
2026-01-27 15:08:56 +0000

    feat: add OutgoingMobilityCnrV2HostProvider

---
+++ ewp-host-plugin-skeleton/src/main/java/pt/ulisboa/ewp/host/plugin/skeleton/provider/omobilities/cnr/OutgoingMobilityCnrV2HostProvider.java
@@ -0,0 +1,41 @@
+package pt.ulisboa.ewp.host.plugin.skeleton.provider.omobilities.cnr;
+
+import java.util.Collection;
+import pt.ulisboa.ewp.host.plugin.skeleton.provider.HostVersionedProvider;
+
+/**
+ * A provider that allows EWP Node to notify a host about a change notification on some outgoing
+ * mobility kept by some partner institution (V2).
+ */
+public abstract class OutgoingMobilityCnrV2HostProvider extends HostVersionedProvider {
+
+  private static final int DEFAULT_MAX_NUMBER_OUTGOING_MOBILITY_IDS_PER_REQUEST = 1;
+
+  /**
+   * Called when a change notification of an outgoing mobility emitted by one of partner
+   * institutions.
+   *
+   * @see <a
+   *     href="https://github.com/erasmus-without-paper/ewp-specs-api-omobility-cnr/tree/stable-v2">EWP
+   *     Specification</a>
+   * @param sendingHeiId Identifier of the HEI which is the sending partner of the outgoing
+   *     mobilities that have been changed.
+   * @param outgoingMobilityIds Outgoing Mobilities IDs that were changed.
+   */
+  public abstract void onChangeNotification(
+      String sendingHeiId, Collection<String> outgoingMobilityIds);
+
+  /**
+   * Indicates the maximum number of Outgoing Mobility IDs that can be handled on any request.
+   *
+   * @return Maximum number of Outgoing Mobility IDs
+   */
+  public int getMaxOutgoingMobilityIdsPerRequest() {
+    return DEFAULT_MAX_NUMBER_OUTGOING_MOBILITY_IDS_PER_REQUEST;
+  }
+
+  @Override
+  public String getVersion() {
+    return "2.0.0";
+  }
+}
